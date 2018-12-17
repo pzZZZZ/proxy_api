d6dc78e1a9715568b6b2addd09814ea43b5af866
pengzhan <pengzhan@mistong.com>
2018-12-17 14:58:40 +0800

    add proxy 豆瓣

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+const router = require('koa-router')()
+const proxy = require('http-proxy-middleware')
+const c2k = require('koa2-connect')
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/string', async (ctx, next) => {
+  ctx.body = 'koa2 string'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+router.get('/api/getFirstData', c2k(proxy({ target: 'https://api.douban.com/v2/movie/in_theaters?apikey=0b2bdeda43b5688921839c8ecb20399b&city=%E5%8C%97%E4%BA%AC&start=0&count=100&client=somemessage&udid=dddddddddddddddddddddd', changeOrigin: true })))
+
+
+module.exports = router
